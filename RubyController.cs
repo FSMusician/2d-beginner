e2706907315240749dd1f470817a8b17e967298a
FSMusician <CJShel0412@gmail.com>
2021-11-15 00:07:01 -0600

    Ruby's Adventure - Dialogue, Audio and Building

--- RubyController.cs
+++ RubyController.cs
@@ -25,18 +25,26 @@ public class RubyController : MonoBehaviour
     Animator animator;
     Vector2 lookDirection = new Vector2(1, 0);
 
+    AudioSource audioSource;
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-
         currentHealth = maxHealth;
-       
+
+        audioSource = GetComponent<AudioSource>();
+
         //Frame Rate Target
         QualitySettings.vSyncCount = 0;
         Application.targetFrameRate = 60;
         
+    }
+
+    public void PlaySound(AudioClip clip)
+    {
+        audioSource.PlayOneShot(clip);
     }
 
     // Update is called once per frame
@@ -67,6 +75,19 @@ public class RubyController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.C))
         {
             Launch();
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, lookDirection, 1.5f, LayerMask.GetMask("NPC"));
+            if (hit.collider != null)
+            {
+                NonPlayerCharacter character = hit.collider.GetComponent<NonPlayerCharacter>();
+                if (character != null)
+                {
+                    character.DisplayDialog();
+                }
+            }
         }
     }  
     
